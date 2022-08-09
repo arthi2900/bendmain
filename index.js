f011bb5731264f0c329576cf02b56fa688d915fd
aaarthi292716 <aaarthi292716@gmail.com>
2022-08-09 17:38:31 +0530

    add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+//app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URL, {
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
+/*
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
@@ -42,7 +42,7 @@ const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
-
+*/
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
